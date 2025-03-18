6973bee8e1f08bb79b7b312fa42c33b6047954fc
신동근 <tlsehdrms124@naver.com>
2025-03-18 16:39:28 +0900

    feat: 초기 세팅 설정

---
+++ src/main/java/com/example/springdbmasterslave/SpringDbMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdbmasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbMasterSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbMasterSlaveApplication.class, args);
+	}
+
+}
