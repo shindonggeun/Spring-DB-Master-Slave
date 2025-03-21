9024659ae3a81da17901b6dcafc802a59770c797
신동근 <tlsehdrms124@naver.com>
2025-03-21 17:44:24 +0900

    feat: DataSource 설정 추가 및 RoutingDataSource 추가

---
+++ BackEnd/src/main/java/com/example/backend/global/config/RoutingDataSource.java
@@ -0,0 +1,18 @@
+package com.example.backend.global.config;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+/**
+ * 실제 커넥션을 가져오는 시점에 호출되어,
+ * 현재 트랜잭션이 readOnly인지 여부에 따라 마스터 또는 슬레이브를 선택하는 라우팅 클래스
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        // 현재 트랜잭션이 readOnly일 경우 → slave 데이터소스로 라우팅
+        return (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) ? "slave"
+            : "master";
+    }
+}
